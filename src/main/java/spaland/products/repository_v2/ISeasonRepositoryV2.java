0c13160ba8f76bdc3258dddd2ed6dbea507776be
logwns <logwns@gmail.com>
2023-03-29 14:14:30 +0900

    delete v2

--- src/main/java/spaland/products/repository_v2/ISeasonRepositoryV2.java
+++
@@ -1,8 +0,0 @@
-package spaland.products.repository_v2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spaland.products.model_v2.SeasonV2;
-
-public interface ISeasonRepositoryV2 extends JpaRepository<SeasonV2, Long> {
-    SeasonV2 findBySeason(String season);
-}
