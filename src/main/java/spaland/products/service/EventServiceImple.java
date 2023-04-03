17458e4d793bec83709f8b6e021c0dacf3920c58
DESKTOP-8ROT1QV\교육생11 <shghdrl1234@gmail.com>
2023-04-03 20:39:08 +0900

    fix : change return value to responseEntity

--- src/main/java/spaland/products/service/EventServiceImple.java
+++ src/main/java/spaland/products/service/EventServiceImple.java
@@ -3,7 +3,11 @@ package spaland.products.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import spaland.Response.Message;
 import spaland.exception.CustomException;
 import spaland.products.model.Event;
 import spaland.products.repository.IEventRepository;
@@ -25,7 +29,7 @@ public class EventServiceImple implements IEventService {
     private final IEventRepository iEventRepository;
 
     @Override
-    public void addEvent(RequestEvent requestEvent) {
+    public ResponseEntity<Message> addEvent(RequestEvent requestEvent) {
         iEventRepository.save(
                 Event.builder()
                         .name(requestEvent.getName())
@@ -38,16 +42,25 @@ public class EventServiceImple implements IEventService {
                         .isDisplay(requestEvent.getIsDisplay())
                         .build()
         );
+
+        Message message = new Message();
+        message.setMessage("이벤트 등록 성공!");
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEvent getEvent(Integer eventId) {
+    public ResponseEntity<Message> getEvent(Integer eventId) {
 
-        return new ModelMapper().map(iEventRepository.findById(eventId).orElseThrow(()-> new CustomException(INVALID_EVENT)), ResponseEvent.class);
+        Message message = new Message();
+        message.setMessage("이벤트 조회 성공!");
+        message.setData(new ModelMapper().map(iEventRepository.findById(eventId).orElseThrow(()-> new CustomException(INVALID_EVENT)), ResponseEvent.class));
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @Override
-    public List<ResponseEvent> getAllEvent() {
+    public ResponseEntity<Message> getAllEvent() {
         List<Event> eventList = iEventRepository.findAll();
         List<ResponseEvent> responseEventList = new ArrayList<>();
 
@@ -60,6 +73,10 @@ public class EventServiceImple implements IEventService {
                     );
                 }
         );
-        return responseEventList;
+        Message message = new Message();
+        message.setMessage("이벤트 전체 조회 성공!");
+        message.setData(responseEventList);
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
--- src/main/java/spaland/products/service/IProductImageService.java
+++ src/main/java/spaland/products/service/IProductImageService.java
@@ -1,5 +1,7 @@
 package spaland.products.service;
 
+import org.springframework.http.ResponseEntity;
+import spaland.Response.Message;
 import spaland.products.model.ProductImage;
 import spaland.products.vo.RequestProductImage;
 import spaland.products.vo.ResponseProductImage;
@@ -7,7 +9,7 @@ import spaland.products.vo.ResponseProductImage;
 import java.util.List;
 
 public interface IProductImageService {
-    void addProductImage(RequestProductImage requestProductImage);
-    ResponseProductImage getProductImage(Long productImageId);
-    List<ResponseProductImage> getAllProductImage();
+    ResponseEntity<Message> addProductImage(RequestProductImage requestProductImage);
+    ResponseEntity<Message> getProductImage(Long productImageId);
+    ResponseEntity<Message> getAllProductImage();
 }
