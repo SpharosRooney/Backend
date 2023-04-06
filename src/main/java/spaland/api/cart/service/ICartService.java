5e511ec374103d7da11fa0ab27704f1377a3e661
minji <kmj99a@gmail.com>
2023-04-06 17:59:52 +0900

    fix : cart delete all

--- src/main/java/spaland/api/cart/service/ICartService.java
+++ src/main/java/spaland/api/cart/service/ICartService.java
@@ -7,13 +7,16 @@ import spaland.api.cart.vo.RequestCartCount;
 import spaland.api.cart.vo.RequestCheckCart;
 import spaland.api.cart.vo.RequestDeleteCart;
 
+import java.util.List;
+
 
 public interface ICartService {
     ResponseEntity<Message> addCart(RequestCart requestCart, String userId);
     ResponseEntity<Message> modifyCart(RequestCartCount requestCartCount, String userId);
-    ResponseEntity<Message> deleteProduct(RequestDeleteCart requestDeleteCart, String userId);
+    ResponseEntity<Message> deleteProduct(String userId);
     ResponseEntity<Message> getAllByUserCart(String userId,Boolean isDelete);
     ResponseEntity<Message> checkboxCart(RequestCheckCart requestCheckCart, String userId, Boolean isDelete);
     ResponseEntity<Message> checkboxAllCart(String userId,Boolean isDelete);
+    ResponseEntity<Message> deleteAllProduct(String userId,Boolean isDelete);
 
 }
